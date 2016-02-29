fb7de0bda1f54f736c6c23ac4503cd7220f1e99f
Ivan Ermilov <earthquakesan@gmail.com>
2016-02-29 12:08:18 +0100

    worker processing SPARQL endpoints now

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 test-static-seed: compile
 	java -cp ./target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.StaticSeedGeneratorCli "ipc://localhost.ipc" &
-	java -cp ./target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.WorkerCli "1" "ipc://localhost.ipc" "ipc://localhostsink.ipc" &
+	java -cp ./target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.WorkerCli "1" "ipc://localhost.ipc" "/tmp/" &
 	java -cp ./target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.ZeroMQBasedFrontierCli "ipc://localhost.ipc" 
 
 test-ckan-seed: compile
 	java -cp ./target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.CkanSeedGeneratorCli "ipc://localhost.ipc" &
-	java -cp ./target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.WorkerCli "1" "ipc://localhost.ipc" "ipc://localhostsink.ipc" &
+	java -cp ./target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.WorkerCli "1" "ipc://localhost.ipc" "/tmp/" &
 	java -cp ./target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.ZeroMQBasedFrontierCli "ipc://localhost.ipc" 
 
 compile:
--- src/main/java/org/aksw/simba/squirrel/cli/WorkerCli.java
+++ src/main/java/org/aksw/simba/squirrel/cli/WorkerCli.java
@@ -2,11 +2,14 @@ package org.aksw.simba.squirrel.cli;
 
 import org.aksw.simba.squirrel.frontier.impl.zeromq.ZeroMQBasedFrontierClient;
 import org.aksw.simba.squirrel.robots.RobotsManagerImpl;
+import org.aksw.simba.squirrel.sink.impl.file.FileBasedSink;
 import org.aksw.simba.squirrel.worker.Worker;
 import org.aksw.simba.squirrel.worker.impl.WorkerImpl;
 
 import crawlercommons.fetcher.http.SimpleHttpFetcher;
 import crawlercommons.fetcher.http.UserAgent;
+
+import java.io.File;
 
 public class WorkerCli {
     public static void main(String[] args) {
@@ -17,9 +20,11 @@ public class WorkerCli {
 
         Integer WORKER_ID = Integer.parseInt(args[0]);
         String FRONTIER_ADDRESS = args[1];
-        String SINK_ADDRESS = args[2];
+        String SINK_FOLDER = args[2];
 
-        Worker worker = new WorkerImpl(ZeroMQBasedFrontierClient.create(FRONTIER_ADDRESS, WORKER_ID), null,
+        FileBasedSink sink = new FileBasedSink(new File(SINK_FOLDER), false);
+
+        Worker worker = new WorkerImpl(ZeroMQBasedFrontierClient.create(FRONTIER_ADDRESS, WORKER_ID), sink,
                 new RobotsManagerImpl(new SimpleHttpFetcher(new UserAgent("Test", "", ""))), 2000);
         System.out.println("Running worker...");
         worker.run();
