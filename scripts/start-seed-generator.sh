f1fd8ba959d77c200af22820b1308bb48b3eb23c
Ivan Ermilov <earthquakesan@gmail.com>
2016-03-03 12:40:52 +0100

    added lodstats seed gen cli

--- scripts/start-seed-generator.sh
+++ scripts/start-seed-generator.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo "Starting Seed Generator. Sending URIs to $1 address"
-java -cp $DIR/../target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.StaticSeedGeneratorCli $1
+java -cp $DIR/../target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.LodstatsSeedGeneratorCli $1
