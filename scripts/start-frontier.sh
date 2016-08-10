abdd5aaac9cd1b182f74a2f8dae915e31d8bb140
Ivan Ermilov <earthquakesan@gmail.com>
2016-08-10 15:32:29 +0200

    added logs output to scripts

--- scripts/start-frontier.sh
+++ scripts/start-frontier.sh
@@ -3,4 +3,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo "Starting Frontier with $1 address"
-java -cp $DIR/../target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.ZeroMQBasedFrontierCli $1
+echo "Logs output to $2"
+java -cp $DIR/../target/squirrel-0.1-jar-with-dependencies.jar org.aksw.simba.squirrel.cli.ZeroMQBasedFrontierCli $1 $2
