219138058b54f21215be7dadafc237261e19190f
mroeder <roeder@informatik.uni-leipzig.de>
2017-07-21 11:44:01 +0200

    Fixed problems introduced by a wrong merging of the RDBQueue class. Added test case for reproducing #17. Fixed #17.

--- src/test/java/org/aksw/simba/squirrel/queue/RDBQueueTest.java
+++ src/test/java/org/aksw/simba/squirrel/queue/RDBQueueTest.java
@@ -1,11 +1,8 @@
 package org.aksw.simba.squirrel.queue;
 
-import com.rethinkdb.model.MapObject;
-import org.aksw.simba.squirrel.data.uri.CrawleableUri;
-import org.aksw.simba.squirrel.data.uri.CrawleableUriFactory4Tests;
-import org.aksw.simba.squirrel.data.uri.UriType;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.net.InetAddress;
 import java.net.URI;
@@ -13,37 +10,35 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import org.aksw.simba.squirrel.data.uri.CrawleableUri;
+import org.aksw.simba.squirrel.data.uri.CrawleableUriFactory4Tests;
+import org.aksw.simba.squirrel.data.uri.UriType;
+import org.junit.Before;
+import org.junit.Test;
 
+import com.rethinkdb.model.MapObject;
+
+@SuppressWarnings("rawtypes")
 public class RDBQueueTest {
     RDBQueue rdbQueue;
     List<CrawleableUri> uris = new ArrayList<CrawleableUri>();
 
     @Before
     public void setUp() throws Exception {
-        //to start up the server run
-        //docker run -p 8080:8080 -p 29015:29015 -p 28015:28015 --name rethinkdb -v "$PWD:/data" -it --rm rethinkdb:2.3.5
-        rdbQueue = new RDBQueue("localhost", 28015);
+        // to start up the server run
+        // docker run -p 8080:8080 -p 29015:29015 -p 28015:28015 --name rethinkdb -v "$PWD:/data" -it --rm rethinkdb:2.3.5
+        // rdbQueue = new RDBQueue("localhost", 28015);
+        rdbQueue = new RDBQueue("192.168.99.100", 28015);
 
         CrawleableUriFactory4Tests cuf = new CrawleableUriFactory4Tests();
-        uris.add(
-            cuf.create(
-                new URI("http://danbri.org/foaf.rdf"),
-                InetAddress.getByName("127.0.0.1"),
-                UriType.DUMP
-            ));
-        uris.add(
-            cuf.create(
-                new URI("http://dbpedia.org/resource/New_York"),
-                InetAddress.getByName("127.0.0.1"),
-                UriType.DEREFERENCEABLE
-            ));
-        uris.add(
-            cuf.create(
-                new URI("http://dbpedia.org/resource/Moscow"),
-                InetAddress.getByName("127.0.0.1"),
-                UriType.DEREFERENCEABLE
-            ));
+        uris.add(cuf.create(new URI("http://localhost/sparql"), InetAddress.getByName("127.0.0.1"), UriType.SPARQL));
+        uris.add(cuf.create(new URI("http://dbpedia.org/resource/New_York"), InetAddress.getByName("127.0.0.1"),
+                UriType.DEREFERENCEABLE));
+        uris.add(cuf.create(new URI("http://dbpedia.org/resource/Moscow"), InetAddress.getByName("127.0.0.1"),
+                UriType.DEREFERENCEABLE));
+        // Added this to check https://github.com/AKSW/Squirrel/issues/17
+        uris.add(cuf.create(new URI("http://danbri.org/foaf.rdf"),
+                InetAddress.getByName((new URI("http://danbri.org/foaf.rdf")).toURL().getHost()), UriType.DUMP));
     }
 
     @Test
@@ -58,20 +53,21 @@ public class RDBQueueTest {
 
     @Test
     public void packTuple() throws Exception {
-        List rArray = rdbQueue.packTuple("/192.168.1.1", "http://localhost");
-        assertTrue(rArray.contains("/192.168.1.1"));
+        List rArray = rdbQueue.packTuple("192.168.1.1", "http://localhost");
+        assertTrue(rArray.contains("192.168.1.1"));
         assertTrue(rArray.contains("http://localhost"));
-        rArray = rdbQueue.packTuple("/192.168.1.1", "DUMP");
-        assertTrue(rArray.contains("/192.168.1.1"));
+        rArray = rdbQueue.packTuple("192.168.1.1", "DUMP");
+        assertTrue(rArray.contains("192.168.1.1"));
         assertTrue(rArray.contains("DUMP"));
     }
 
     @Test
     public void getIpAddressTypeKey() throws Exception {
         List rArray = rdbQueue.getIpAddressTypeKey(uris.get(0));
-        assertTrue(rArray.contains("/127.0.0.1"));
-        assertFalse(rArray.contains("http://danbri.org/foaf.rdf"));
-        assertTrue(rArray.contains("DUMP"));
+        String arrayString = rArray.toString();
+        assertTrue(arrayString, rArray.contains("127.0.0.1"));
+        assertFalse(arrayString, rArray.contains("http://danbri.org/foaf.rdf"));
+        assertTrue(arrayString, rArray.contains("SPARQL"));
     }
 
     @Test
@@ -87,10 +83,14 @@ public class RDBQueueTest {
     @Test
     public void purgeQueue() throws Exception {
         rdbQueue.open();
-        rdbQueue.addToQueue(uris.get(0));
-        assertTrue(rdbQueue.length() == 1);
         rdbQueue.purgeQueue();
-        assertTrue(rdbQueue.length() == 0);
+        assertEquals(0, rdbQueue.length());
+        for (CrawleableUri uri : uris) {
+            rdbQueue.addToQueue(uri);
+        }
+        assertEquals(3, rdbQueue.length());
+        rdbQueue.purgeQueue();
+        assertEquals(0, rdbQueue.length());
         rdbQueue.close();
     }
 
@@ -99,20 +99,20 @@ public class RDBQueueTest {
         rdbQueue.open();
         rdbQueue.purgeQueue();
         rdbQueue.addCrawleableUri(uris.get(1));
-        assertTrue(rdbQueue.length() == 1);
+        assertEquals(1, rdbQueue.length());
         List iatKey = rdbQueue.getIpAddressTypeKey(uris.get(2));
         rdbQueue.addCrawleableUri(uris.get(2), iatKey);
-        assertTrue(rdbQueue.length() == 1);
+        assertEquals(1, rdbQueue.length());
         rdbQueue.close();
     }
 
     @Test
     public void addToQueue() throws Exception {
         rdbQueue.open();
-        for(CrawleableUri uri : uris) {
+        for (CrawleableUri uri : uris) {
             rdbQueue.addToQueue(uri);
         }
-        assertTrue(rdbQueue.length() == 2);
+        assertEquals(3, rdbQueue.length());
         rdbQueue.close();
     }
 
@@ -122,18 +122,18 @@ public class RDBQueueTest {
         assertTrue(rHashMap.containsKey("uris"));
         assertTrue(rHashMap.containsKey("ipAddress"));
         assertTrue(rHashMap.containsKey("type"));
-        assertTrue(rHashMap.get("ipAddress").equals("/127.0.0.1"));
-        assertTrue(rHashMap.get("type").equals("DUMP"));
+        assertEquals("127.0.0.1", rHashMap.get("ipAddress"));
+        assertEquals("SPARQL", rHashMap.get("type"));
     }
 
     @Test
     public void getIterator() throws Exception {
         rdbQueue.open();
-        for(CrawleableUri uri : uris) {
+        for (CrawleableUri uri : uris) {
             rdbQueue.addToQueue(uri);
         }
         Iterator<IpUriTypePair> iter = rdbQueue.getIterator();
-        while(iter.hasNext()) {
+        while (iter.hasNext()) {
             IpUriTypePair pair = iter.next();
             System.out.println(pair.toString());
         }
@@ -143,14 +143,14 @@ public class RDBQueueTest {
     @Test
     public void getUris() throws Exception {
         rdbQueue.open();
-        for(CrawleableUri uri : uris) {
+        for (CrawleableUri uri : uris) {
             rdbQueue.addToQueue(uri);
         }
         Iterator<IpUriTypePair> iter = rdbQueue.getIterator();
-        while(iter.hasNext()) {
+        while (iter.hasNext()) {
             IpUriTypePair pair = iter.next();
             List<CrawleableUri> uriList = rdbQueue.getUris(pair);
-            for(CrawleableUri uri : uriList) {
+            for (CrawleableUri uri : uriList) {
                 assertTrue(uris.contains(uri));
             }
         }
