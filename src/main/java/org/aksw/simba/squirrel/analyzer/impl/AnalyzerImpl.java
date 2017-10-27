2480c3f856b3e8b3cdeb1525efedfa5e14fb5d34
Micha <michael.roeder@uni-paderborn.de>
2017-10-27 14:39:16 +0200

    Updated Makefile. Reworked the analyzer interface as discussed before. Implemented the usage of streaming in the RDF analyzer. Adapted the WorkerImpl accordingly.

--- src/main/java/org/aksw/simba/squirrel/analyzer/impl/AnalyzerImpl.java
+++ src/main/java/org/aksw/simba/squirrel/analyzer/impl/AnalyzerImpl.java
@@ -1,66 +1,60 @@
 package org.aksw.simba.squirrel.analyzer.impl;
 
 import java.io.File;
-import java.net.URI;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.ArrayList;
+import java.io.FileInputStream;
 import java.util.Iterator;
 
 import org.aksw.simba.squirrel.analyzer.Analyzer;
 import org.aksw.simba.squirrel.data.uri.CrawleableUri;
 import org.aksw.simba.squirrel.sink.Sink;
-import org.aksw.simba.squirrel.sink.collect.SimpleUriCollector;
 import org.aksw.simba.squirrel.sink.collect.UriCollector;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
-import org.apache.jena.shared.JenaException;
+import org.apache.http.HttpHeaders;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFLanguages;
+import org.apache.tika.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnalyzerImpl implements Analyzer {
-	
-	private URI uri;
-	private Model model;
-	private Sink sink;
-	
-	public AnalyzerImpl(CrawleableUri curi, File data, Sink sink) {
-		this.uri = curi.getUri();
-		this.model = ModelFactory.createDefaultModel().read(data.getPath());
-		this.sink = sink;
-	}
-	
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnalyzerImpl.class);
 
-	@Override
-	public Iterator<String> analyze() {
-		
-		CrawleableUri curi = new CrawleableUri(uri);
-		
-		
-		UriCollector collector = new SimpleUriCollector(sink);
-		
+    private UriCollector collector;
 
-		try {
-	       
-	        StmtIterator si;
-	        si = model.listStatements();
+    public AnalyzerImpl(UriCollector collector) {
+        this.collector = collector;
+    }
 
-	        while(si.hasNext()) {
-	            Statement s=si.nextStatement();
-	            sink.addTriple(curi, s.asTriple());
-	            collector.addTriple(curi, s.asTriple());
+    @Override
+    public Iterator<String> analyze(CrawleableUri curi, File data, Sink sink) {
+        FileInputStream fin = null;
+        try {
+            // First, try to get the language of the data
+            Lang lang = null;
+            String contentType = (String) curi.getData(HttpHeaders.CONTENT_TYPE);
+            if (contentType != null) {
+                lang = RDFLanguages.contentTypeToLang(contentType);
+            } else {
+                lang = RDFLanguages.filenameToLang(data.getName(), null);
+            }
+            // Read the file and iterate its triples
+            fin = new FileInputStream(data);
+            Iterator<Triple> tripleIter = RDFDataMgr.createIteratorTriples(fin, null, "");
+            Triple t;
+            while (tripleIter.hasNext()) {
+                t = tripleIter.next();
+                sink.addTriple(curi, t);
+                collector.addTriple(curi, t);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Exception while analyzing. Aborting.");
+        } finally {
+            IOUtils.closeQuietly(fin);
+        }
 
-	        }
-	    }
-	    catch(JenaException | NoSuchElementException c) {}
-		    
-		return collector.getUris();
-	}
-	
+        return collector.getUris();
+    }
 
 }
