05b69439d0ae39725411536759aa3baf2dc35673
Ivan Ermilov <earthquakesan@gmail.com>
2017-12-05 15:12:55 +0100

    FTPFetcher test, can not fetch simple URI

---
+++ src/test/java/org/aksw/simba/squirrel/fetcher/ftp/FTPFetcherTest.java
@@ -0,0 +1,25 @@
+package org.aksw.simba.squirrel.fetcher.ftp;
+
+import org.aksw.simba.squirrel.data.uri.CrawleableUri;
+import org.aksw.simba.squirrel.data.uri.CrawleableUriFactory4Tests;
+import org.aksw.simba.squirrel.data.uri.CrawleableUriFactoryImpl;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URI;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FTPFetcherTest {
+    @Test
+    public void fetch() throws Exception {
+        CrawleableUri danbriUri = new CrawleableUri(new URI("http://danbri.org/foaf.rdf"));
+        FTPFetcher fetcher = new FTPFetcher();
+        File data = fetcher.fetch(danbriUri);
+        assertNotNull(data);
+    }
+
+}
