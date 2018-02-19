2e46555f16a665e4bf287701b52fafad79056525
varunmaitreya <varun.maitreya@gmail.com>
2018-02-19 16:33:53 +0100

    extending method for visualization of crawled graph

--- src/main/java/org/aksw/simba/squirrel/sink/impl/GraphQuery.java
+++ src/main/java/org/aksw/simba/squirrel/sink/impl/GraphQuery.java
@@ -1,11 +1,15 @@
 package org.aksw.simba.squirrel.sink.impl;
 
-import org.apache.jena.query.Query;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
+    import com.sun.xml.internal.fastinfoset.util.StringArray;
+    import org.apache.jena.graph.Node;
+    import org.apache.jena.graph.Triple;
+    import org.apache.jena.query.*;
+    import org.apache.jena.rdf.model.RDFNode;
+    import org.apache.jena.shared.JenaException;
+    import org.apache.xerces.xs.StringList;
+
+    import java.util.ArrayList;
+    import java.util.List;
 
 
 public class GraphQuery extends Query {
@@ -18,7 +22,7 @@ public class GraphQuery extends Query {
             "  ?subject ?predicate ?object\n" +
             "  }\n" +
             "}";
-            //"LIMIT 25"; //for specifying the number of triples for large datasets
+        //"LIMIT 25"; //for specifying the number of triples for large datasets
         Query query = QueryFactory.create(queryString) ;
 
         //for debugging the query
@@ -28,5 +32,35 @@ public class GraphQuery extends Query {
         QueryExecution qexec = QueryExecutionFactory.sparqlService("http://localhost:3030/lolo/query", query);
         ResultSet results = qexec.execSelect();
         ResultSetFormatter.out(System.out, results, query) ;
+        results.getResultVars();
+
+
+    }
+
+    public static List<Triple> GetAllTriplesFromGraph(String [] args) {
+        List<Triple> results = new ArrayList<>();
+        String queryString = "SELECT ?subject ?predicate ?object\n" +
+            "WHERE {\n" +
+            "  GRAPH <http://localhost:3030/lolo/data/" + args[0] + ">\n" + //specific graph should be selected in the dataset
+            "  {\n" +
+            "  ?subject ?predicate ?object\n" +
+            "  }\n" +
+            "}";
+        //"LIMIT 25"; //for specifying the number of triples for large datasets
+        Query query = QueryFactory.create(queryString);
+
+        //for debugging the query
+        System.out.println(queryString);
+
+        //sparql endpoint for specific dataset
+        QueryExecution qexec = QueryExecutionFactory.sparqlService("http://localhost:3030/lolo/query", query);
+        ResultSet queryResults = qexec.execSelect();
+        while (queryResults.hasNext()) {
+            QuerySolution qs = queryResults.nextSolution();
+            results.add(new Triple(qs.get("?subject").asNode(), qs.get("?predicate").asNode(), qs.get("?object").asNode()));
+        }
+        qexec.close();
+
+        return results;
     }
 }
