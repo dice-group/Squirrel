f58419bcd3761e9dffe058f0508ffe16f8b67591
Geraldo <gsjunior@mail.uni-paderborn.de>
2018-03-06 18:46:04 +0100

    initial commit

---
+++ src/main/java/org/aksw/simba/squirrel/analyzer/htmlscraper/YamlFileAtributes.java
@@ -0,0 +1,25 @@
+package org.aksw.simba.squirrel.analyzer.htmlscraper;
+
+public class YamlFileAtributes {
+	
+	private YamlFileAtributes() {
+		
+	}
+	
+	protected static final String SEARCH_CHECK = "check";
+	protected static final String SEARCH_CHECK_URL = "url";
+	protected static final String SEARCH_CHECK_TERMS = "terms";
+	
+	protected static final String SEARCH_RESULT = "search-result";
+	protected static final String SEARCH_RESULT_SELECTOR = "selector";
+	protected static final String SEARCH_RESULT_ACTION = "action";
+	
+	protected static final String DATASET_RULES = "rules";
+	protected static final String DATASET_RULES_DOWNLOADLINK = "download-link";
+	
+	protected static final String DOWNLOAD_PAGE_RESOURCES = "resources";
+	
+
+	
+
+}
