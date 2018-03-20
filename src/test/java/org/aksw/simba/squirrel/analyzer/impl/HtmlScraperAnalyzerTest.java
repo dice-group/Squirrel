f66e9759c1dfdadcb05c3dfcc3b25f3e3241b490
gsjunior <gsjunior@localhost>
2018-03-20 05:31:21 -0300

    changes in the test, now using local file

--- src/test/java/org/aksw/simba/squirrel/analyzer/impl/HtmlScraperAnalyzerTest.java
+++ src/test/java/org/aksw/simba/squirrel/analyzer/impl/HtmlScraperAnalyzerTest.java
@@ -5,23 +5,10 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
-import org.aksw.simba.squirrel.analyzer.Analyzer;
 import org.aksw.simba.squirrel.analyzer.htmlscraper.HtmlScraper;
-import org.aksw.simba.squirrel.collect.SimpleUriCollector;
-import org.aksw.simba.squirrel.collect.SqlBasedUriCollector;
-import org.aksw.simba.squirrel.collect.UriCollector;
 import org.aksw.simba.squirrel.data.uri.CrawleableUri;
-import org.aksw.simba.squirrel.data.uri.UriUtils;
-import org.aksw.simba.squirrel.data.uri.serialize.Serializer;
-import org.aksw.simba.squirrel.data.uri.serialize.java.GzipJavaUriSerializer;
-import org.aksw.simba.squirrel.sink.Sink;
-import org.aksw.simba.squirrel.sink.impl.mem.InMemorySink;
-import org.aksw.simba.squirrel.utils.TempFileHelper;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
@@ -32,36 +19,60 @@ import org.junit.Test;
 public class HtmlScraperAnalyzerTest {
 	
 	private File configurationFile;
-	File fetchedFile;
-	private Analyzer analyzer;
-	
-	private UriCollector collector;
-	private Sink sink;
+	private File fetchedFile;
+	private List<Triple> expectedTriplesMcloudDetail;
+
 	private HtmlScraper scraper;
-	private Serializer serializer;
+
 
 	
 	@Before
 	public void prepareGeneral() throws URISyntaxException, IOException {
-		String dbdir = TempFileHelper.getTempDir("dbTest", "").getAbsolutePath() + File.separator + "test";
-		serializer = new GzipJavaUriSerializer();
-		this.sink = new InMemorySink();
-		this.collector = SqlBasedUriCollector.create(serializer, dbdir);
-
 		configurationFile = new File("src/test/resources/html_scraper_analyzer/yaml");
 		scraper = new HtmlScraper(configurationFile);
-		analyzer = new HTMLScraperAnalyzer(collector,scraper);
-
 	}
 	
+	@Before
+	public void prepareDetailMcloud() {
+		expectedTriplesMcloudDetail = new ArrayList<Triple>();
+		Node s = NodeFactory.createURI("https://www.mcloud.de/web/guest/suche/-/results/detail/verkehrslageaufautobahnenschleifenhamburg");
+		Node downloadUrl = NodeFactory.createBlankNode("schema:downloadUrl");
+		expectedTriplesMcloudDetail.add(new Triple(s, downloadUrl
+				, NodeFactory.createURI("http://geodienste.hamburg.de/HH_WFS_Verkehr_opendata?SERVICE=WFS&REQUEST=GetFeature&VERSION=1.1.0&TYPENAME=bab_vkl")));
+		
+		expectedTriplesMcloudDetail.add(new Triple(s, downloadUrl
+				, NodeFactory.createURI("http://geodienste.hamburg.de/HH_WMS_Verkehr_opendata?REQUEST=GetCapabilities&SERVICE=WMS")));
+		
+		expectedTriplesMcloudDetail.add(new Triple(s, downloadUrl
+				, NodeFactory.createURI("http://geoportal-hamburg.de/verkehrsportal/")));
+		
+		expectedTriplesMcloudDetail.add(new Triple(s, downloadUrl
+				, NodeFactory.createURI("http://geodienste.hamburg.de/HH_WFS_Verkehr_opendata?REQUEST=GetCapabilities&SERVICE=WFS")));
+		
+		expectedTriplesMcloudDetail.add(new Triple(s, NodeFactory.createBlankNode("rdfs:label")
+				, NodeFactory.createLiteral("Verkehrslage auf Autobahnen (Schleifen) Hamburg")));
+		
+		expectedTriplesMcloudDetail.add(new Triple(s, NodeFactory.createBlankNode("rdfs:comment")
+				, NodeFactory.createLiteral("Darstellung der Verkehrslage auf Autobahnen, die auf Grundlage von Schleifendaten ermittelt und erzeugt wird.")));
+		expectedTriplesMcloudDetail.add(new Triple(s, NodeFactory.createBlankNode("schema:provider")
+				, NodeFactory.createURI("http://www.hamburg.de/bwvi/verkehr-strassenwesen/")));
+		
+		expectedTriplesMcloudDetail.add(new Triple(s, NodeFactory.createBlankNode("schema:license")
+				, NodeFactory.createURI("https://www.govdata.de/dl-de/by-2-0")));
+		
+	}
 	
 	@Test
-	public void scrapMcloud() throws Exception {
+	public void scrapDetailMcloud() throws Exception {
 		CrawleableUri curi = new CrawleableUri(new URI("https://www.mcloud.de/web/guest/suche/-/results/detail/verkehrslageaufautobahnenschleifenhamburg"));
-		fetchedFile = new File("src/test/resources/html_scraper_analyzer/mcloudexample.html");
-		 Iterator<byte[]> iterator = analyzer.analyze(curi, fetchedFile, sink);
+		fetchedFile = new File("src/test/resources/html_scraper_analyzer/mcloud/mcloud_detail.html");
 		
-		 System.out.println(UriUtils.getDomainName(curi.getUri().toString()));
+		 List<Triple> listTriples = new ArrayList<Triple>();
+		 listTriples.addAll(scraper.scrape(curi.getUri().toString(), fetchedFile));
+		
+		Assert.assertEquals(expectedTriplesMcloudDetail, listTriples);
+		 
+		
 		
 	}
 	
