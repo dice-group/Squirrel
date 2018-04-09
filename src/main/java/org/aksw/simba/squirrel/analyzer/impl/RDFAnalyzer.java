9b7e39849bc6f3bea08794803a358f46e4579e80
gsjunior <gsjunior@localhost>
2018-04-09 13:41:46 -0300

    refactor of mimetype detection solution

--- src/main/java/org/aksw/simba/squirrel/analyzer/impl/RDFAnalyzer.java
+++ src/main/java/org/aksw/simba/squirrel/analyzer/impl/RDFAnalyzer.java
@@ -2,18 +2,26 @@ package org.aksw.simba.squirrel.analyzer.impl;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.aksw.simba.squirrel.Constants;
 import org.aksw.simba.squirrel.analyzer.Analyzer;
 import org.aksw.simba.squirrel.collect.UriCollector;
 import org.aksw.simba.squirrel.data.uri.CrawleableUri;
 import org.aksw.simba.squirrel.sink.Sink;
+import org.apache.jena.atlas.logging.Log;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFLanguages;
+import org.apache.jena.riot.system.StreamRDF;
 import org.apache.jena.riot.system.StreamRDFBase;
+import org.apache.jena.sparql.core.Quad;
 import org.apache.tika.Tika;
 import org.apache.tika.io.IOUtils;
 import org.slf4j.Logger;
@@ -22,12 +30,25 @@ import org.slf4j.LoggerFactory;
 public class RDFAnalyzer implements Analyzer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RDFAnalyzer.class);
-    private Tika tika = new Tika();
 
     private UriCollector collector;
+    
+    private List<Lang> listLangs = new ArrayList<Lang>();
+    
+    
 
     public RDFAnalyzer(UriCollector collector) {
         this.collector = collector;
+        listLangs.add(Lang.NT);
+        listLangs.add(Lang.NQUADS);
+        listLangs.add(Lang.RDFJSON);
+        listLangs.add(Lang.RDFTHRIFT);
+        listLangs.add(Lang.RDFXML);
+        listLangs.add(Lang.JSONLD);
+        listLangs.add(Lang.TRIG);
+        listLangs.add(Lang.TRIX);
+        listLangs.add(Lang.TTL);
+        listLangs.add(Lang.TURTLE);
     }
 
     @Override
@@ -37,14 +58,41 @@ public class RDFAnalyzer implements Analyzer {
             // First, try to get the language of the data
             Lang lang = null;
             String contentType = (String) curi.getData(Constants.URI_HTTP_MIME_TYPE_KEY);
-            String type = tika.detect(data);
+            StreamRDF filtered = new FilterSinkRDF(curi, sink, collector);
             if (contentType != null) {
                 lang = RDFLanguages.contentTypeToLang(contentType);
+                RDFDataMgr.parse(filtered, data.getAbsolutePath(), lang);
             } else {
-                lang = RDFLanguages.filenameToLang(data.getName(), null);
+            	for(Lang l : listLangs) {
+            		try {
+            			System.out.println(data.getAbsolutePath());
+            			RDFDataMgr.parse(filtered, data.getAbsolutePath(), l);
+            			break;
+            		}catch(Exception e) {
+            			
+            			LOGGER.warn("Could not parse file as " + l.getName());
+            		}
+            		
+            	}
+            	
+//                InputStream is = new FileInputStream(data);
+//                lang = RDFLanguages.contentTypeToLang(tika.detect(is));
+//                try {
+//                	RDFDataMgr.parse(filtered, data.getAbsolutePath(), lang);
+//                }catch(Exception e) {
+//                	if(Lang.NTRIPLES.equals(lang)) {
+//	                	LOGGER.warn("Could not parse file as N-Triples. Trying N-Quads...");
+//	                	RDFDataMgr.parse(filtered, data.getAbsolutePath(), Lang.N3);
+//	                } else 
+//	                {
+//	                	throw e;
+//	                }   	
+//                }finally {
+//					is.close();
+//				}
             }
-            FilterSinkRDF filtered = new FilterSinkRDF(curi, sink, collector);
-            RDFDataMgr.parse(filtered, data.getAbsolutePath(), lang);
+            
+            
         } catch (Exception e) {
             LOGGER.error("Exception while analyzing. Aborting. ", e);
         } finally {
@@ -67,8 +115,14 @@ public class RDFAnalyzer implements Analyzer {
 
         @Override
         public void triple(Triple triple) {
-            sink.addTriple(curi, triple);
+        	sink.addTriple(curi, triple);
             collector.addTriple(curi, triple);
+        }
+        
+        @Override
+        public void quad(Quad quad) {
+        	sink.addTriple(curi, quad.asTriple());
+            collector.addTriple(curi, quad.asTriple());
         }
 
     }
