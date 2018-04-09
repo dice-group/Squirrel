b8154f7366b98c0982aae33c6b96d5b7bbc7cb59
gsjunior <gsjunior@localhost>
2018-04-09 13:32:35 -0300

    tests all the different mimetypes

--- src/test/java/org/aksw/simba/squirrel/analyzer/impl/RDFAnalyzerTest.java
+++ src/test/java/org/aksw/simba/squirrel/analyzer/impl/RDFAnalyzerTest.java
@@ -1,20 +1,21 @@
 package org.aksw.simba.squirrel.analyzer.impl;
 
 import java.io.File;
-import java.net.InetAddress;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
-import java.util.Iterator;
+import java.util.List;
 
 import org.aksw.simba.squirrel.analyzer.Analyzer;
 import org.aksw.simba.squirrel.collect.SimpleUriCollector;
 import org.aksw.simba.squirrel.collect.UriCollector;
 import org.aksw.simba.squirrel.data.uri.CrawleableUri;
-import org.aksw.simba.squirrel.data.uri.UriType;
+import org.aksw.simba.squirrel.data.uri.serialize.Serializer;
 import org.aksw.simba.squirrel.data.uri.serialize.java.GzipJavaUriSerializer;
 import org.aksw.simba.squirrel.sink.Sink;
 import org.aksw.simba.squirrel.sink.impl.mem.InMemorySink;
+import org.aksw.simba.squirrel.utils.TempPathUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,46 +23,67 @@ import org.junit.Test;
 public class RDFAnalyzerTest {
 
 	private Analyzer analyzer;
-	private CrawleableUri curi;
 	private UriCollector collector;
 	private Sink sink;
-	private File data;
-	private String uriToFetch = "http://dbpedia.org/resource/New_York";
-	private String fileToTest = "rdf_analyzer/genders_en.ttl";
-	private int expectedUris = 2829;
+	private Serializer serializer = new GzipJavaUriSerializer();
+	private long minimum_expected_new_york = 8603;
+	private long expected_genders_en = 8408;
+//	private String fileToTest = "rdf_analyzer/genders_en";
+//	private String fileToTest = "rdf_analyzer/genders_en.tql";
 
 
 	@Before
 	public void prepare() throws URISyntaxException, UnknownHostException {
-
 		this.sink = new InMemorySink();
-		this.collector = new SimpleUriCollector(new GzipJavaUriSerializer());
-
-		analyzer = new RDFAnalyzer(collector);
-
-		curi = new CrawleableUri(new URI(uriToFetch));
-		curi.setIpAddress(InetAddress.getByName("dbpedia.org"));
-		curi.setType(UriType.DEREFERENCEABLE);
-
-		sink.openSinkForUri(curi);
-		collector.openSinkForUri(curi);
-
-		ClassLoader classLoader = getClass().getClassLoader();
-
-		data = new File(classLoader.getResource(fileToTest).getFile());
 	}
 
 
 	@Test
-	public void test() {
-		Iterator<byte[]> uris =  analyzer.analyze(curi, data, sink);
-
-		int cont = 0;
-		while(uris.hasNext()) {
-			cont ++;
-			uris.next();
+	public void test_genders_en() throws URISyntaxException {
+		CrawleableUri curi = new CrawleableUri(new URI("http://aksw.test.org/genders_en"));
+		
+		ClassLoader classLoader = getClass().getClassLoader();
+		File filesFolder = new File(classLoader.getResource("rdf_analyzer/genders_en").getFile());
+		List<File> listFiles =  TempPathUtils.searchPath4Files(filesFolder);
+		
+		for(File file : listFiles) {
+			
+			this.collector = new SimpleUriCollector(serializer);
+			sink.openSinkForUri(curi);
+			collector.openSinkForUri(curi);
+			
+			analyzer = new RDFAnalyzer(collector);
+			analyzer.analyze(curi, file, sink);
+			
+			Assert.assertEquals(expected_genders_en, ((SimpleUriCollector) this.collector).getSize());
+			
 		}
+	}
+	
+	@Test
+	public void test_new_york() throws URISyntaxException, IOException {
 
-		Assert.assertEquals(expectedUris, cont);
+		CrawleableUri curi = new CrawleableUri(new URI("http://dbpedia.org/resource/New_York"));
+		
+		ClassLoader classLoader = getClass().getClassLoader();
+		File filesFolder = new File(classLoader.getResource("rdf_analyzer/new_york").getFile());
+		List<File> listFiles =  TempPathUtils.searchPath4Files(filesFolder);
+		
+		for(File file : listFiles) {
+			
+			this.collector = new SimpleUriCollector(serializer);
+			sink.openSinkForUri(curi);
+			collector.openSinkForUri(curi);
+			
+			analyzer = new RDFAnalyzer(collector);
+			analyzer.analyze(curi, file, sink);
+			
+			sink.closeSinkForUri(curi);
+			collector.closeSinkForUri(curi);
+			
+			Assert.assertEquals(minimum_expected_new_york, ((SimpleUriCollector) this.collector).getSize());
+			
+		}
+		
 	}
 }
