7764807e8faa6876ebc057064ccf27598392a79a
varunmaitreya <varun.maitreya@gmail.com>
2018-04-23 10:39:09 +0200

    created new branch for feature ckan crawler

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 function wait_for_it()
 {
     local serviceport=$1
@@ -17,7 +18,7 @@ function wait_for_it()
         echo "[$i/$max_try] ${service}:${port} is still not available; giving up after ${max_try} tries. :/"
         exit 1
       fi
-      
+
       echo "[$i/$max_try] try in ${retry_seconds}s once again ..."
       let "i++"
       sleep $retry_seconds
