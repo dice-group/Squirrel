1ca0e0bfa44ed2eda40f9af856851c2d47efbe58
varunmaitreya <varun.maitreya@gmail.com>
2018-06-04 00:15:38 +0200

    get http header data and print json and header data

--- src/main/java/org/aksw/simba/squirrel/ckancrawler/ckanaggregator/testing/request.py
+++ src/main/java/org/aksw/simba/squirrel/ckancrawler/ckanaggregator/testing/request.py
@@ -2,5 +2,11 @@ import urllib2
 import json
 
 req = urllib2.urlopen('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=dcj2IihqQUe0XunKypsXv9vJBChxNg2Dud6er4Ae&location=Denver+CO')
-http = json.loads(html)
+
+http = req.read()
 print http
+
+httphead = req.headers
+print httphead
+
+
