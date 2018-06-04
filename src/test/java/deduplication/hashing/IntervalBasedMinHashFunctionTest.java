336b0e2f2fc5a078d8e498a4f05921fbfb11629f
ph1l1pF <philip.frerk@icloud.com>
2018-06-04 13:59:07 +0200

    continued on hash tests

--- src/test/java/deduplication/hashing/IntervalBasedMinHashFunctionTest.java
+++ src/test/java/deduplication/hashing/IntervalBasedMinHashFunctionTest.java
@@ -1,6 +1,5 @@
 package deduplication.hashing;
 
-import org.aksw.simba.squirrel.deduplication.hashing.HashValue;
 import org.aksw.simba.squirrel.deduplication.hashing.impl.IntervalBasedMinHashFunction;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
@@ -21,28 +20,52 @@ public class IntervalBasedMinHashFunctionTest {
     }
 
     @Test
-    public void testHashing() {
+    public void testListOrder() {
 
         // create two lists of triples with same triples but in different order
         // => hash values must be equal
-        String subject = "s";
-        String predicate = "p";
-        String object = "o";
-        Triple t = Triple.create(
-            NodeFactory.createURI(subject),
-            NodeFactory.createURI(predicate),
-            NodeFactory.createURI(object));
-
-        List<Triple> tripleList1 = new ArrayList<>();
-        tripleList1.add(t);
+        List<Triple> tripleList1 = generateNonBlankTriples(5);
         List<Triple> tripleList2 = new ArrayList<>();
+        for (int i = tripleList1.size() - 1; i >= 0; i++) {
+            tripleList2.add(tripleList1.get(i));
+        }
 
-        HashValue hashValue1 = hashFunction.hash(tripleList1);
-        HashValue hashValue2 = hashFunction.hash(tripleList2);
-
-        Assert.assertTrue(hashValue1.equals(hashValue2));
-
+        Assert.assertTrue(hashFunction.hash(tripleList1).equals(hashFunction.hash(tripleList2)));
     }
 
+    @Test
+    public void testBlankNode() {
+
+        // create two lists of triples with same content, but list 2 has one blank node
+        // => blank node must be ignored, so hash values must be equal
+
+        List<Triple> tripleList1 = generateNonBlankTriples(1);
+        List<Triple> tripleList2 = new ArrayList<>(tripleList1);
+        Triple tripleWithBlankNode = Triple.create(
+            NodeFactory.createBlankNode("subject blank"),
+            NodeFactory.createURI("predicate"),
+            NodeFactory.createURI("object"));
+        tripleList2.add(tripleWithBlankNode);
+
+        Assert.assertTrue(hashFunction.hash(tripleList1).equals(hashFunction.hash(tripleList2)));
+    }
+
+    /**
+     * Generate some triples (without blank nodes).
+     *
+     * @param amount The amount of the triples to create.
+     * @return The generated list of triples.
+     */
+    private List<Triple> generateNonBlankTriples(int amount) {
+        List<Triple> triples = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            Triple t = Triple.create(
+                NodeFactory.createURI("subject " + i),
+                NodeFactory.createURI("predicate " + i),
+                NodeFactory.createURI("object " + i));
+            triples.add(t);
+        }
+        return triples;
+    }
 
 }
