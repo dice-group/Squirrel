2d0ccc96b4e7de4562e296c0090ffad5cb58108d
Micha <michael.roeder@uni-paderborn.de>
2018-06-27 15:24:36 +0200

    Fixed FTPFetcherTest.

--- src/test/java/org/aksw/simba/squirrel/fetcher/ftp/FTPFetcherTest.java
+++ src/test/java/org/aksw/simba/squirrel/fetcher/ftp/FTPFetcherTest.java
@@ -1,30 +1,25 @@
 package org.aksw.simba.squirrel.fetcher.ftp;
 
-import org.aksw.simba.squirrel.data.uri.CrawleableUri;
-import org.aksw.simba.squirrel.data.uri.CrawleableUriFactory4Tests;
-import org.aksw.simba.squirrel.data.uri.CrawleableUriFactoryImpl;
-import org.aksw.simba.squirrel.uri.processing.UriProcessor;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertNull;
 
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
-import java.util.List;
 
-import static org.junit.Assert.*;
+import org.aksw.simba.squirrel.data.uri.CrawleableUri;
+import org.junit.Before;
+import org.junit.Test;
 
 public class FTPFetcherTest {
 
-	private UriProcessor uriProcessor;
-	private CrawleableUri uri;
+//	private UriProcessor uriProcessor;
+//	private CrawleableUri uri;
 
 	@Before
 	public void prepare() throws UnknownHostException, URISyntaxException {
 
-		uriProcessor = new UriProcessor();
+//		uriProcessor = new UriProcessor();
 		// TODO: this test takes ages
 		// TODO: Use mock FTP server here (docker or Java mock server)
 //		uri = uriProcessor.recognizeInetAddress(new CrawleableUri(new URI("ftp://ftp.ncbi.nlm.nih.gov/pubchem/RDF")));
@@ -35,7 +30,7 @@ public class FTPFetcherTest {
     @Test
     public void shouldNotFetchHTTPLinks() throws Exception {
         FTPFetcher fetcher = new FTPFetcher();
-        File data = fetcher.fetch(uri);
+        File data = fetcher.fetch(new CrawleableUri(new URI("http://dbpedia.org/resource/New_York")));
         fetcher.close();
         assertNull(data);
     }
