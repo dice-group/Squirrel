fb40474899f1eab40cf801caff8abb6e974419ba
abhihc <abhishekhc1993@gmail.com>
2019-01-23 23:16:15 +0100

    Fixed null pointer execption.

--- squirrel.worker/src/test/java/org/dice_research/squirrel/analyzer/impl/MimeTypeDetectorTest.java
+++ squirrel.worker/src/test/java/org/dice_research/squirrel/analyzer/impl/MimeTypeDetectorTest.java
@@ -2,6 +2,7 @@ package org.dice_research.squirrel.analyzer.impl;
 
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFLanguages;
+import org.apache.jena.vocabulary.RDF;
 import org.dice_research.squirrel.analyzer.mime.MimeTypeDetector;
 import org.dice_research.squirrel.analyzer.mime.TypeDetector;
 import org.junit.Before;
@@ -10,9 +11,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 
@@ -41,10 +42,18 @@ public class MimeTypeDetectorTest {
         classLoader = getClass().getClassLoader();
     }
 
-
-    private Lang validate(String fileName) throws NullPointerException {
-        File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
-        return typeDetector.detectMimeType(file);
+    private Lang validate(String fileName) {
+        if(fileName == null)
+            return RDFLanguages.RDFNULL;
+        else {
+            URL url = classLoader.getResource(fileName);
+            if(url != null) {
+                File file = new File(url.getFile());
+                return typeDetector.detectMimeType(file);
+            }
+            else
+                return RDFLanguages.RDFNULL;
+        }
     }
 
     @Parameterized.Parameters
@@ -56,7 +65,8 @@ public class MimeTypeDetectorTest {
             {"rdf_analyzer/new_york/new_york_turtle", RDFLanguages.TURTLE},
             {"Sample_Files/sample.nt", RDFLanguages.NTRIPLES},
             {"rdf_analyzer/new_york/new_york_rdfjson", RDFLanguages.RDFJSON},
-            {"rdf_analyzer/new_york/new_york_jsonld", RDFLanguages.JSONLD}
+            {"rdf_analyzer/new_york/new_york_jsonld", RDFLanguages.JSONLD},
+            {null, RDFLanguages.RDFNULL}
         });
     }
 
