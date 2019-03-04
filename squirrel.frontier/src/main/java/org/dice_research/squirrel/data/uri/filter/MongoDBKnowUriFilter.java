8f7bc9f06047c0b62ebd39c9b60ca443d2d0729a
gsjunior86 <gsjunior86@uni-paderborn.de>
2019-03-04 12:36:20 +0100

    checks if mongoConfiguration is null

--- squirrel.frontier/src/main/java/org/dice_research/squirrel/data/uri/filter/MongoDBKnowUriFilter.java
+++ squirrel.frontier/src/main/java/org/dice_research/squirrel/data/uri/filter/MongoDBKnowUriFilter.java
@@ -68,7 +68,7 @@ public class MongoDBKnowUriFilter implements KnownUriFilter, Cloneable, Closeabl
     	MongoClientOptions.Builder optionsBuilder = MongoClientOptions.builder();
         MongoConfiguration mongoConfiguration = MongoConfiguration.getMDBConfiguration();
 	
-		if(mongoConfiguration.getConnectionTimeout() != null && mongoConfiguration.getSocketTimeout() != null && mongoConfiguration.getServerTimeout() != null) {
+		if(mongoConfiguration != null &&(mongoConfiguration.getConnectionTimeout() != null && mongoConfiguration.getSocketTimeout() != null && mongoConfiguration.getServerTimeout() != null)) {
 			optionsBuilder.connectTimeout(mongoConfiguration.getConnectionTimeout());
 			optionsBuilder.socketTimeout(mongoConfiguration.getSocketTimeout());
 			optionsBuilder.serverSelectionTimeout(mongoConfiguration.getServerTimeout());
