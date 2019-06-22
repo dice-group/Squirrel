d448acfb10b655e6f8baf815dc89d01812c404de
CatherineChiramel <chiramelcatherine@gmail.com>
2019-06-22 13:34:44 +0200

    Testcase for featureHashing method

--- squirrel.frontier/src/test/java/org/dice_research/squirrel/predictor/impl/PredictorImplTest.java
+++ squirrel.frontier/src/test/java/org/dice_research/squirrel/predictor/impl/PredictorImplTest.java
@@ -4,6 +4,7 @@ import de.jungblut.math.activation.SigmoidActivationFunction;
 import de.jungblut.math.dense.DenseDoubleVector;
 import de.jungblut.online.regression.RegressionClassifier;
 import de.jungblut.online.regression.RegressionModel;
+import org.dice_research.squirrel.Constants;
 import org.dice_research.squirrel.data.uri.CrawleableUri;
 import org.dice_research.squirrel.predictor.PredictorImpl;
 import org.junit.Assert;
@@ -11,13 +12,43 @@ import org.junit.Test;
 
 import java.net.URI;
 
+import static org.junit.Assert.assertEquals;
+
 public class PredictorImplTest {
     private CrawleableUri curi;
     private PredictorImpl predictor;
 
     @Test
     public void featureHashing() throws Exception {
-
+        int flag1 = 0;
+        int flag2 = 0;
+        CrawleableUri uri1 = new CrawleableUri(new URI("https://dbpedia.org/resource/New_York"));
+        CrawleableUri uri2 = new CrawleableUri(new URI("https://wikipedia.org/resource/New_York"));
+        CrawleableUri uri3 = new CrawleableUri(new URI("abc:///xyz/zyx/lmn.uvw"));
+        PredictorImpl predictor = new PredictorImpl();
+        predictor.featureHashing(uri1);
+        predictor.featureHashing(uri2);
+        predictor.featureHashing(uri3);
+        Object feature1 = uri1.getData(Constants.FEATURE_VECTOR);
+        Object feature2 = uri2.getData(Constants.FEATURE_VECTOR);
+        Object feature3 = uri3.getData(Constants.FEATURE_VECTOR);
+        double[] featureArray1 = ((double[]) feature1);
+        double[] featureArray2 = ((double[]) feature2);
+        double[] featureArray3 = ((double[]) feature3);
+        for(int i=0; i<featureArray2.length; i++){
+            if(featureArray2[i] == featureArray1[i]) {
+                if(featureArray1[i] != 0.0) {
+                    flag1 = 1;
+                }
+            }
+            if(featureArray1[i] == featureArray3[i] ){
+                if(featureArray1[i]!= 0.0){
+                    flag2 = 1;
+                }
+            }
+        }
+        assertEquals(1, flag1);
+        assertEquals(0, flag2);
     }
 
     @Test
@@ -40,7 +71,7 @@ public class PredictorImplTest {
 
         // Prediction
         double pred = predictor.predict(curi);
-        Assert.assertEquals(1d, pred, 1e-4);
+        assertEquals(1d, pred, 1e-4);
 
     }
 
