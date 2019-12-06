4f3a75c5e2ee517852a46ee7536dea2ddb7f4bc0
Micha <michael.roeder@uni-paderborn.de>
2019-12-06 14:43:28 +0100

    Fxied the RegexBased filter to actually use regex expressions.

--- squirrel.frontier/src/main/java/org/dice_research/squirrel/data/uri/filter/RegexBasedWhiteListFilter.java
+++ squirrel.frontier/src/main/java/org/dice_research/squirrel/data/uri/filter/RegexBasedWhiteListFilter.java
@@ -1,17 +1,17 @@
 package org.dice_research.squirrel.data.uri.filter;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedHashSet;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
+import org.apache.commons.io.FileUtils;
 import org.dice_research.squirrel.data.uri.CrawleableUri;
-import org.dice_research.squirrel.data.uri.filter.AbstractKnownUriFilterDecorator;
-import org.dice_research.squirrel.data.uri.filter.KnownUriFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,72 +21,59 @@ public class RegexBasedWhiteListFilter extends AbstractKnownUriFilterDecorator {
 
     public static RegexBasedWhiteListFilter create(KnownUriFilter decorated, File whitelistfile) {
         try {
-            Set<String> whiteList = loadWhiteList(whitelistfile);
-            return new RegexBasedWhiteListFilter(decorated, whiteList);
+            List<String> whiteList = FileUtils.readLines(whitelistfile, StandardCharsets.UTF_8);
+            Set<Pattern> whitePatterns = new HashSet<Pattern>();
+            if (whiteList != null) {
+                for (String s : whiteList) {
+                    if ((s != null) && (!s.isEmpty())) {
+                        try {
+                            whitePatterns.add(Pattern.compile(s));
+                        } catch (PatternSyntaxException e) {
+                            LOGGER.error("Got an incorrect regex pattern. It will be ignored.", e);
+                        }
+                    }
+                }
+            }
+            return new RegexBasedWhiteListFilter(decorated, whitePatterns);
         } catch (IOException e) {
             LOGGER.error("A problem was found when loading the WhiteList");
         }
         return null;
     }
 
-    private Set<String> whiteList;
+    private Set<Pattern> whiteList;
 
-    public RegexBasedWhiteListFilter(KnownUriFilter decorated, Set<String> whiteList) {
+    public RegexBasedWhiteListFilter(KnownUriFilter decorated, Set<Pattern> whiteList) {
         super(decorated);
         this.whiteList = whiteList;
     }
 
     @Override
     public boolean isUriGood(CrawleableUri uri) {
-        if (super.isUriGood(uri) && (whiteList == null || whiteList.isEmpty())) {
-            return true;
+        if (!super.isUriGood(uri)) {
+            return false;
         }
-
-        else if (super.isUriGood(uri) && whiteList != null && !whiteList.isEmpty()) {
-
-            for (String s : whiteList) {
-
-                Pattern p = Pattern.compile(s.toLowerCase());
+        if (whiteList == null || whiteList.isEmpty()) {
+            return true;
+        } else {
+            for (Pattern p : whiteList) {
                 Matcher m = p.matcher(uri.getUri().toString().toLowerCase());
-
                 if (m.find()) {
                     return true;
                 }
-
             }
-
         }
         return false;
     }
 
-    protected static Set<String> loadWhiteList(File whiteListFile) throws IOException {
-        Set<String> list = new LinkedHashSet<String>();
-
-        FileReader fr = new FileReader(whiteListFile);
-        BufferedReader br = new BufferedReader(fr);
-
-        String line;
-
-        while ((line = br.readLine()) != null) {
-            list.add(line);
-        }
-
-        br.close();
-
-        return list;
-    }
-
-
     @Override
     public void add(CrawleableUri uri, long timestamp) {
         super.add(uri, timestamp);
-
     }
 
     @Override
     public void open() {
-        // TODO Auto-generated method stub
-        
+        // nothing to do
     }
 
 }
