d4d5f50ecef0491f128cf60e65c6987cd6929a5f
param-jot <lotey2110@gmail.com>
2019-12-13 20:40:41 +0100

    Added maven dependency and modified graph query to generate graphs according to domains

--- squirrel.crawled-data-graph/src/main/java/org/dice_research/squirrel/QueryExecFactoryConnection.java
+++ squirrel.crawled-data-graph/src/main/java/org/dice_research/squirrel/QueryExecFactoryConnection.java
@@ -18,6 +18,7 @@ import org.apache.jena.ext.com.google.common.net.InternetDomainName;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.sparql.core.DatasetDescription;
+import org.neo4j.driver.v1.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,15 +26,16 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class QueryExecFactoryConnection {
     private static final Logger LOGGER = LoggerFactory.getLogger(QueryExecFactoryConnection.class);
-
     /**
      * The Query factory used to query the SPARQL endpoint.
      */
     protected static QueryExecutionFactory queryExecFactory = null;
+
 
     public QueryExecFactoryConnection(QueryExecutionFactory queryExecFactory, UpdateExecutionFactory updateExecFactory) {
         this.queryExecFactory = queryExecFactory;
@@ -101,18 +103,19 @@ public class QueryExecFactoryConnection {
         }
         return domainLevels;
     }
-    public static StringBuilder getPath(String fullPath) {
+    public static String getPath(String fullPath) {
         String[] path = fullPath.split("/");
         List<String> a = new ArrayList<String>();
         for (String s : path) {
             a.add(s);
         }
         StringBuilder sb2 = new StringBuilder();
-        for(int i=0;i<a.size();i++) {
+        for(int i=0;i<2;i++) {
             sb2.append( a.get(i) + "/");
-            LOGGER.info(String.valueOf(sb2));
         }
-        return sb2;
+        String p1 = sb2.toString();
+        LOGGER.info(p1);
+        return p1;
 
     }
 
@@ -126,30 +129,47 @@ public class QueryExecFactoryConnection {
         while (rs.hasNext()) {
             QuerySolution sol = rs.nextSolution();
             RDFNode uri = sol.get("uri");
-            LOGGER.info("URI: " + uri);
             URI url = new URI(uri.toString());
-
-            // URI parent = url.getPath().endsWith("/") ? url.resolve("..") : url.resolve(".");
-
             String hostname = url.getHost();
             String segments = url.getPath();
             String urlQuery = url.getQuery();
             String baseDomain = InternetDomainName.from(hostname).topPrivateDomain().toString();
             LOGGER.info(baseDomain);
-            String fullPath = baseDomain+segments;
+            String fullPath = baseDomain + segments;
             String subdomain = hostname.replace("." + baseDomain, "");
             LOGGER.info(subdomain);
 
             getDomainLevels(hostname);
             getPath(fullPath);
-
             hostname = String.valueOf(InternetDomainName.from(hostname).topPrivateDomain());
             InternetDomainName it = InternetDomainName.from(hostname);
             String domainname = String.valueOf(it.publicSuffix());
-            String temp = hostname.replaceAll("." + domainname, "");
-            LOGGER.info("domain name: "+temp);
+            String domainName = hostname.replaceAll("." + domainname, "");
+            LOGGER.info(domainName);
 
+            Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("cgraph", "neo4j"));
+            try (Session session = driver.session()) {
+                HashMap<String, Object> parameters = new HashMap<String, Object>();
+                parameters.put("nameOfDomain", domainName);
+                parameters.put("baseDomain", baseDomain);
+                parameters.put("subDomain", subdomain);
+                parameters.put("path", "dbpdeia.org/resource");
+                parameters.put("value", "value");
+                parameters.put("uri", url.toString());
+
+                String cypherQuery=
+                    "CREATE (m1:data1 { Node1:$nameOfDomain}),(m2:data2 {Node2:$baseDomain}),(m3:data3 {Node3:$subDomain}), " +
+                        "(m4:data4 {Node4:$path}),(m5:data5 {Node5:$value}),(m6:data6 {Node6:$uri})  "+
+                        "CREATE (m1)-[r1:baseDomain]->(m2), (m1)-[r2:subDomain]->(m3), (m1)-[r3:path]->(m4), (m1)-[r4:value]->(m5), (m1)-[r5:uri]->(m6)\n" +
+                        "RETURN m1,m2,m3,m4,m5,m6,r1,r2,r3,r4,r5";
+                StatementResult result = session.run(cypherQuery, parameters);
+            }
+            driver.close();
         }
         qe.close();
+
     }
+
+
 }
+
