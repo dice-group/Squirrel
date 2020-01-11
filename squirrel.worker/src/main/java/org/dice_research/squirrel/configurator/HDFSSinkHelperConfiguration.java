f2e14c0b8138c19c2bdebdb9d6d1fb17bb28a413
anirudhash <aka.anirudh@gmail.com>
2020-01-12 00:37:17 +0100

    Slight modification

--- squirrel.worker/src/main/java/org/dice_research/squirrel/configurator/HDFSSinkHelperConfiguration.java
+++ squirrel.worker/src/main/java/org/dice_research/squirrel/configurator/HDFSSinkHelperConfiguration.java
@@ -7,26 +7,26 @@ public class HDFSSinkHelperConfiguration extends Configuration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HDFSSinkHelperConfiguration.class);
 
-    public static final String HDFS_REMOTE_HOST_KEY = "HDFS_REMOTE_HOST";
+    public static final String HDFS_REMOTE_KEY = "HDFS_REMOTE_HOST";
     public static final String HDFS_DESTINATION_DIRECTORY_KEY = "HDFS_DESTINATION_DIRECTORY";
 
-    private String hdfshost;
+    private String hdfsHost;
     private String destinationDirectory;
 
     public HDFSSinkHelperConfiguration(String remotehost, String destdirectory) {
-        this.hdfshost = remotehost;
+        this.hdfsHost = remotehost;
         this.destinationDirectory = destdirectory;
     }
 
     public static HDFSSinkHelperConfiguration getHDFSHelperConfguration() throws Exception {
-        String hdfshost = getEnv(HDFS_REMOTE_HOST_KEY, LOGGER);
+        String hdfshost = getEnv(HDFS_REMOTE_KEY, LOGGER);
         String destinationdirectory = getEnv(HDFS_DESTINATION_DIRECTORY_KEY, LOGGER);
         if (hdfshost != null) {
-            LOGGER.info("The worker will use " + HDFS_REMOTE_HOST_KEY + " as the HDFS host.");
+            LOGGER.info("The worker will use " + HDFS_REMOTE_KEY + " as the HDFS host.");
             LOGGER.info("The worker will use " + HDFS_DESTINATION_DIRECTORY_KEY + " as the output directory.");
             return new HDFSSinkHelperConfiguration(hdfshost, destinationdirectory);
         } else {
-            String msg = "Couldn't get " + HDFS_REMOTE_HOST_KEY + " from the environment. "+
+            String msg = "Couldn't get " + HDFS_REMOTE_KEY + " from the environment. "+
                 "Couldn't get " + HDFS_DESTINATION_DIRECTORY_KEY + " from the environment. ";
             LOGGER.error(msg);
             throw new Exception(msg);
@@ -34,7 +34,7 @@ public class HDFSSinkHelperConfiguration extends Configuration {
     }
 
     public String getHDFSHost(){
-        return hdfshost;
+        return hdfsHost;
     }
 
     public String getDestinationdirectory() {
