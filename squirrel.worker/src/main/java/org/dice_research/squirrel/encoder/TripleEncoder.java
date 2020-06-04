9ebf9c9b1b4ad7e6839b1bcb90b389f1ef73cb30
Geraldo <gsjunior86@gmail.com>
2020-06-04 13:18:59 +0200

    using Jena Util URIref class

--- squirrel.worker/src/main/java/org/dice_research/squirrel/encoder/TripleEncoder.java
+++ squirrel.worker/src/main/java/org/dice_research/squirrel/encoder/TripleEncoder.java
@@ -2,14 +2,11 @@ package org.dice_research.squirrel.encoder;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
+import org.apache.jena.util.URIref;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -59,44 +56,47 @@ public class TripleEncoder {
 		if(!n.isURI())
 			return n;
 		
-		Map<String, String> parameters = getUriParameters(n.getURI());
 		
-		if(parameters.isEmpty())
-			return n;
+		return NodeFactory.createURI(URIref.encode(n.toString()));
 		
-		String baseURI = n.toString().substring(0,n.toString().indexOf("?"));
-		
-		
-		URIBuilder uriBuilder = new URIBuilder(baseURI);
-		for(Entry<String, String> param: parameters.entrySet())
-			uriBuilder.addParameter(param.getKey(), param.getValue());
-		
-		return NodeFactory.createURI(uriBuilder.toString());
+//		Map<String, String> parameters = getUriParameters(n.getURI());
+//		
+//		if(parameters.isEmpty())
+//			return n;
+//		
+//		String baseURI = n.toString().substring(0,n.toString().indexOf("?"));
+//		
+//		
+//		URIBuilder uriBuilder = new URIBuilder(baseURI);
+//		for(Entry<String, String> param: parameters.entrySet())
+//			uriBuilder.addParameter(param.getKey(), param.getValue());
+//		
+//		return NodeFactory.createURI(uriBuilder.toString());
 	}
 
 
-	private Map<String, String> getUriParameters(String uri) throws UnsupportedEncodingException {
-		Map<String, String> mapParameters = new LinkedHashMap<String, String>();
-		if(uri.indexOf("?") == -1)
-			return mapParameters;
-		try {
-		String query = uri.substring(uri.indexOf("?") + 1);
-		String[] pairs = query.split("&");
-		for (String pair : pairs) {
-			int idx = pair.indexOf("=");
-			mapParameters.put(pair.substring(0, idx),
-					pair.substring(idx + 1));
-		}
-		}catch (IndexOutOfBoundsException e) {
-			return new LinkedHashMap<String, String>();
-		}
-
-		return mapParameters;
-	}
+//	private Map<String, String> getUriParameters(String uri) throws UnsupportedEncodingException {
+//		Map<String, String> mapParameters = new LinkedHashMap<String, String>();
+//		if(uri.indexOf("?") == -1)
+//			return mapParameters;
+//		try {
+//		String query = uri.substring(uri.indexOf("?") + 1);
+//		String[] pairs = query.split("&");
+//		for (String pair : pairs) {
+//			int idx = pair.indexOf("=");
+//			mapParameters.put(pair.substring(0, idx),
+//					pair.substring(idx + 1));
+//		}
+//		}catch (IndexOutOfBoundsException e) {
+//			return new LinkedHashMap<String, String>();
+//		}
+//
+//		return mapParameters;
+//	}
 	
 	public static void main(String[] args) throws UnsupportedEncodingException, URISyntaxException {
 		TripleEncoder te = new TripleEncoder();
-		System.out.println(te.encodeUri(NodeFactory.createURI("https://ckan.govdata.de/")).toString());
+		System.out.println(te.encodeUri(NodeFactory.createURI("https://ckan.govdata.de?alalao eee")).toString());
 	}
 
 }
