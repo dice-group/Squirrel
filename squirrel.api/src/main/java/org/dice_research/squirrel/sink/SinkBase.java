4964d611413fb4c18e8713fbee4a24a9c13cf2e3
abhinavhegde <hegde.abhinav@yahoo.com>
2020-06-22 13:40:13 +0200

    Refactored according to the comments on PR #144

--- squirrel.api/src/main/java/org/dice_research/squirrel/sink/SinkBase.java
+++ squirrel.api/src/main/java/org/dice_research/squirrel/sink/SinkBase.java
@@ -5,7 +5,7 @@ import org.dice_research.squirrel.data.uri.CrawleableUri;
 /**
  * This interface defines the basic functionality of all sinks, i.e., they can
  * be opened and closed for a given URI.
- * 
+ *
  * @author Michael R&ouml;der (michael.roeder@uni-paderborn.de)
  *
  */
@@ -13,14 +13,14 @@ public interface SinkBase {
 
     /**
      * Opens the sink to process data for the given URI.
-     * 
+     *
      * @param uri
      *            the URI for which data should be stored.
      */
     public void openSinkForUri(CrawleableUri uri);
 
     /**
-     * Makes sure the data of the given URI is sent to the database.
+     * Flushes this sink for the given URI by writing any buffered data to the underlying storage.
      *
      * @param uri
      *            the URI for which data needs to be sent.
@@ -30,7 +30,7 @@ public interface SinkBase {
 
     /**
      * Closes the resources necessary for storing the data of the given URI.
-     * 
+     *
      * @param uri
      *            the URI for which data has been stored and for which the resources
      *            should be freed.
