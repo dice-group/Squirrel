5a79b24ffbdcef9f2b80fc816eb002b4af69a901
Geraldo <gsjunior86@gmail.com>
2020-07-08 17:42:20 +0200

    updated javadoc

--- squirrel.api/src/main/java/org/dice_research/squirrel/data/uri/filter/UriFilterComposer.java
+++ squirrel.api/src/main/java/org/dice_research/squirrel/data/uri/filter/UriFilterComposer.java
@@ -11,8 +11,19 @@ package org.dice_research.squirrel.data.uri.filter;
 
 public interface UriFilterComposer extends UriFilter {
 	
+	/**
+	 * Returnsthe KnowUriFilter from this {@link UriFilterComposer}
+	 * 
+	 * @return KnownUriFilter
+	 */
 	public KnownUriFilter getKnownUriFilter();
 	
+	
+	/**
+	 * Set the KnowUriFilter for this {@link UriFilterComposer}
+	 * 
+	 * @param knownUriFilter
+	 */
 	public void setKnownUriFilter(KnownUriFilter knownUriFilter);
 
 }
