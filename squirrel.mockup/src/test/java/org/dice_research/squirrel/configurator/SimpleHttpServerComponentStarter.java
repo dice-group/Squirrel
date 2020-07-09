26e2eeeaecd4d3ca0e7cab90e4734f5ffc48c318
Stefan Birkner <mail@stefan-birkner.de>
2020-07-09 21:42:55 +0200

    Fix SimpleHttpServerComponentStarter
    The EnvironmentsVariable rule can only be used within a JUnit 4 test
    class because it needs JUnit 4 for managing its lifecycle. System Lambda
    is a replacement for System Rules that does not need JUnit 4 anymore.
    Therefore we can use it in SimpleHttpServerComponentStarter.

--- squirrel.mockup/src/test/java/org/dice_research/squirrel/configurator/SimpleHttpServerComponentStarter.java
+++ squirrel.mockup/src/test/java/org/dice_research/squirrel/configurator/SimpleHttpServerComponentStarter.java
@@ -2,20 +2,20 @@ package org.dice_research.squirrel.configurator;
 
 import org.hobbit.core.run.ComponentStarter;
 import org.junit.Ignore;
-import org.junit.contrib.java.lang.system.EnvironmentVariables;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.withEnvironmentVariable;
 
 @Ignore
 public class SimpleHttpServerComponentStarter {
 
-    public static final EnvironmentVariables environmentVariables = new EnvironmentVariables();
-
-    public static void main(String[] args) {
-        environmentVariables.set("RESOURCE_MODEL", "../deployment/scenarios/2/nodeA.ttl");
-        environmentVariables.set("ROBOTS_TXT", "../deployment/scenarios/2/robotsA.txt");
-        environmentVariables.set("RESOURCE_MODEL_LANG", "N3");
-        environmentVariables.set("PORT", "8080");
-        environmentVariables.set("USE_DEREF", "true");
-        
-        ComponentStarter.main(new String[] { "org.dice_research.squirrel.components.SimpleHttpServerComponent" });
+    public static void main(String[] args) throws Exception {
+        withEnvironmentVariable("RESOURCE_MODEL", "../deployment/scenarios/2/nodeA.ttl")
+            .and("ROBOTS_TXT", "../deployment/scenarios/2/robotsA.txt")
+            .and("RESOURCE_MODEL_LANG", "N3")
+            .and("PORT", "8080")
+            .and("USE_DEREF", "true")
+            .execute(() ->
+                ComponentStarter.main(new String[] { "org.dice_research.squirrel.components.SimpleHttpServerComponent" })
+            );
     }
 }
